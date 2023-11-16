cbe3ffcd84f7e7a917b2b0d98558c7f41a72c137
phooky <phooky@gmail.com>
2023-11-16 09:55:55 -0500

    removing joystick code; keeping system state stuff in for now but might remove

--- firmware/Main.cc
+++ firmware/Main.cc
@@ -31,7 +31,8 @@
 
 void reset(bool hard_reset) {
     ATOMIC_BLOCK(ATOMIC_FORCEON) {
-        uint8_t resetFlags = MCUSR & 0x0f;
+        // TODO investigate below
+        // uint8_t resetFlags = MCUSR & 0x0f;
         // check for brown out reset flag and report if true
 
         // clear watch dog timer and re-enable
@@ -45,81 +46,11 @@ void reset(bool hard_reset) {
 }
 
 typedef enum {
-    SS_JOYSTICK,
+    SS_GCODE,
     SS_OFF,
 } SystemState;
 
 SystemState ss = SS_OFF;
-
-const int JS_CENTER_VAL = 509;
-const int JS_LOW_VAL = 405;
-const int JS_HIGH_VAL = 615;
-const int JS_DEAD_ZONE = 10;
-const int JS_MAX_ZONE = 10;
-unsigned int js_positions[2] = {0xffff, 0xffff};
-unsigned char js_chan = 0; // current channel being read
-
-const int VEL_SCALE = 300;
-
-int16_t vel_from_js(int axis, bool inverted = false) {
-    cli();
-    int pos = js_positions[axis];
-    sei();
-    if (pos < (JS_LOW_VAL + JS_MAX_ZONE))
-        pos = JS_LOW_VAL;
-    if (pos > (JS_HIGH_VAL + JS_MAX_ZONE))
-        pos = JS_HIGH_VAL;
-    pos -= JS_CENTER_VAL;
-    if ((pos > -JS_DEAD_ZONE) && (pos < JS_DEAD_ZONE)) {
-        pos = 0;
-    } else {
-        if (pos > 0)
-            pos -= JS_DEAD_ZONE;
-        else
-            pos += JS_DEAD_ZONE;
-    }
-    if (inverted) {
-        pos = -pos;
-    }
-    return VEL_SCALE * pos;
-}
-
-void init_analog() {
-    // Clear MUX5 in ADCSRB.
-    ADCSRB &= ~(1 << MUX5);
-    // Set prescaler bits ADPS in ADCSRA.
-    // Try to get 200KHz. 16M/200K = 80x. 128x works.
-    // ADCSRA: ADPS2-0 = 111b
-    // ADCSRA: ADEN = 1b
-    ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
-}
-
-void start_analog_conversion(unsigned char channel) {
-    // Write channel to ADMUX.
-    // ADMUX: REFS1/0 = b01 (AVCC VREF)
-    // ADMUX: ADLAR = 0 (ADCH/L are right-justified)
-    // ADMUX: MUX4-0 = ADCX channel (ADC0-ADC7)
-    ADMUX = (1 << REFS0) | channel;
-    // ADSC is set to start conversion. This bit is cleared when the
-    // conversion is done.
-    // ADIE is set to trigger completion interrupt.
-    ADCSRA |= 1 << ADIE | 1 << ADSC;
-}
-
-/// Perform a blocking analog read on PF0-2
-unsigned int read_analog(unsigned char channel) {
-    // See comments on start_analog_conversion for details on how we
-    // set these registers.
-    ADMUX = (1 << REFS0) | channel;
-    ADCSRA |= 1 << ADSC;
-    while (ADCSRA & (1 << ADSC)) {
-    } // poll until analog read completes.
-    // Result is in ADCH and ADCL. ADCL *must* be read first (if more than
-    // 8 bits are needed.
-    unsigned int result = ADCL;
-    result |= (ADCH & 0x03) << 8;
-    return result;
-}
 
 void init_timers() {
     // NB: Timer2 is unused, this is just nonsense for now
@@ -147,10 +78,6 @@ LiquidCrystalSerial lcd(LCD_STROBE, LCD_DATA, LCD_CLK);
 void start_state(SystemState newss) {
     // shutdown previous mode
     switch (ss) {
-    case SS_JOYSTICK:
-        steppers::enable(0, false);
-        steppers::enable(1, false);
-        break;
     default:
         break;
     }
@@ -168,16 +95,7 @@ void start_state(SystemState newss) {
         lcd.setCursor(0, 2);
         lcd.writeString("turn on joystick control.");
         break;
-    case SS_JOYSTICK:
-        steppers::enable(0, true);
-        steppers::enable(1, true);
-        RGB_LED::setCustomColor(0x99, 0x10, 0x10);
-        lcd.clear();
-        lcd.writeString("Joystick control.");
-        lcd.setCursor(0, 1);
-        lcd.writeString("Press center button to");
-        lcd.setCursor(0, 2);
-        lcd.writeString("turn off steppers.");
+    case SS_GCODE:
         break;
     }
 }
@@ -233,8 +151,6 @@ int main() {
     steppers::setPotValue(X_POT_PIN, 40);
     steppers::setPotValue(Y_POT_PIN, 80);
     init_timers();
-    init_analog();
-    start_analog_conversion(js_chan);
     sei();
     RGB_LED::init();
     RGB_LED::setCustomColor(0x10, 0x88, 0x10);
@@ -267,26 +183,13 @@ int main() {
         ButtonArray::scan();
         if (ButtonArray::pressed() & CENTER)
             switch (ss) {
-            case SS_JOYSTICK:
+            case SS_GCODE:
                 start_state(SS_OFF);
                 break;
             case SS_OFF:
-                start_state(SS_JOYSTICK);
+                start_state(SS_GCODE);
                 break;
             }
-
-        if (ss == SS_JOYSTICK) {
-            // lcd.clear();
-            // lcd.writeString("vx:");
-            int v1 = vel_from_js(0);
-            // lcd.writeInt(v1);
-            steppers::set_velocity(0, v1);
-            // lcd.setCursor(0,1);
-            // lcd.writeString("vy:");
-            int v2 = vel_from_js(1);
-            // lcd.writeInt(v2);
-            steppers::set_velocity(1, v2);
-        }
     }
     return 0;
 }
@@ -297,12 +200,4 @@ ISR(TIMER5_COMPA_vect) {
     // Handle stepper interrupt.
     steppers::do_interrupt();
     // intdbg++;
-}
-
-ISR(ADC_vect) {
-    unsigned int result = ADCL;
-    result |= (ADCH & 0x03) << 8;
-    js_positions[js_chan] = result;
-    js_chan ^= 0x01;
-    start_analog_conversion(js_chan);
 }
