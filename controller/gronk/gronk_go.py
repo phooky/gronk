013bb298916550bff8fb51a7a04a23d7fdbcd6d1
phooky <phooky@gmail.com>
2024-04-18 11:40:00 -0400

    break out and clean up display code. We don't need to sleep constantly.

--- controller/gronk/gronk_go.py
+++ controller/gronk/gronk_go.py
@@ -2,14 +2,12 @@
 
 import RPi.GPIO as GPIO
 import time
-import gpiozero
 from enum import Enum
 from gronklib import Gronk
 from gronkbuttons import Buttons
+from gronkdisplay import Display
 from queue import Queue
 from threading import Thread, Event, Timer
-from PIL import Image, ImageDraw, ImageFont
-from epd2in13_V2 import EPD
 import subprocess
 from multiprocessing.connection import Listener, Client
 
@@ -27,49 +25,13 @@ mode = Mode.IDLE
 
 gronk = Gronk('/dev/ttyACM0')
 
-epd = EPD()
-# resolution is 250x122
+display = Display()
 
-font_logo = ImageFont.truetype('./fonts/ArchivoBlack-Regular.ttf',26)
-font_txt = ImageFont.truetype('./fonts/static/ArchivoNarrow-Bold.ttf',16)
 
 # Todo -- status should return pen state!
 pendown = None # pen starts in indeterminate state; assume down
 
-def show_ready(path = None):
-    epd.init(epd.FULL_UPDATE)
-    epd.Clear(0xff)
-    image = Image.new('1', (epd.height,epd.width),255)
-    draw = ImageDraw.Draw(image)
-    draw.text((0,0),'gronk is ready.', font=font_logo, fill=0)
-    if path:
-        draw.text((0,25),f'SET prints {path}', font=font_txt, fill=0)
-    else:
-        draw.text((0,25),'Arrow keys to jog', font=font_txt, fill=0)
-    draw.text((0,45),'TEST toggles pen up/down', font=font_txt, fill=0)
-    draw.text((0,65),'Upload files at http://gronk.lan/', font=font_txt, fill=0)
-    draw.text((15,85),'Hold "ONLINE" button', font=font_txt, fill=0)
-    draw.text((15,105),'for 5 seconds to shut down.', font=font_txt, fill=0)
-    epd.display(epd.getbuffer(image))
-    epd.sleep()
-
-def show_goodbye():
-    epd.init(epd.FULL_UPDATE)
-    epd.Clear(0xff)
-    image = Image.new('1', (epd.height,epd.width),255)
-    draw = ImageDraw.Draw(image)
-    draw.text((10,5),'goodbye, gronk.', font=font_logo, fill=0)
-    draw.text((0,40),'Please wait thirty seconds before', font=font_txt, fill=0)
-    draw.text((0,60),'switching off.', font=font_txt, fill=0)
-    draw.text((0,80),'Expect to wait about 120 seconds for', font=font_txt, fill=0)
-    draw.text((0,100),'gronk to boot after turning back on.', font=font_txt, fill=0)
-
-    epd.display(epd.getbuffer(image))
-    epd.sleep()
-
-
-
-show_ready()
+display.show_ready()
 
 shutdown_timer = None
 running = Event()
@@ -90,7 +52,7 @@ def file_thread():
             cur_file = c.recv()
             if c == "":
                 return
-            show_ready(cur_file[0])
+            display.show_ready(cur_file[0])
         except Exception as e:
             print("Exception ",e)
 
@@ -208,7 +170,7 @@ finally:
 
 if full_shutdown:
     print("Full shutdown")
-    show_goodbye()
+    display.show_goodbye()
     time.sleep(2);
     subprocess.Popen(['sudo','shutdown','-h','now'])
 
---
+++ controller/gronk/gronkdisplay.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+from PIL import Image, ImageDraw, ImageFont
+from epd2in13_V2 import EPD
+from threading import Lock, Timer
+
+# Display is waveshare 2.13" eHAT; documented here:
+# https://www.waveshare.com/wiki/2.13inch_e-Paper_HAT_Manual
+# Resolution is 250x122
+
+class Display:
+    def __init__(self):
+        self.epd = EPD()
+        self.sleeping = True
+        self.lock = Lock()
+        self.sleep_timeout = 300; # 5m without activity to sleep
+        self.sleep_timer = None
+        self.dim = (self.epd.height,self.epd.width) # EPD module has w/h confused :P
+        self.image = Image.new('1', self.dim ,255)
+        self.draw = ImageDraw.Draw(self.image)
+        self.font_logo = ImageFont.truetype('./fonts/ArchivoBlack-Regular.ttf',26)
+        self.font_txt = ImageFont.truetype('./fonts/static/ArchivoNarrow-Bold.ttf',16)
+        
+    def do_sleep(self):
+        "Put display to sleep after timer runs out."
+        with self.lock:
+            self.sleep_timer = None
+            self.sleeping = True
+            self.epd.sleep()
+            
+    def update(self, clear=False):
+        "Update from buffer and start sleep timer."
+        with self.lock:
+            # we have a potential race condition here, but at worst it just results
+            # in an unecessary sleep() call at the end so it's not worth handling.
+            if self.sleep_timer:
+                self.sleep_timer.cancel()
+                self.sleep_timer = None
+            if self.sleeping:
+                self.epd.init(self.epd.FULL_UPDATE)
+                clear = True
+                self.sleeping = False
+            if clear:
+                self.epd.Clear(0xff)
+            self.epd.display(self.epd.getbuffer(self.image))
+            self.sleep_timer = Timer(self.sleep_timeout,self.do_sleep)
+
+    def clear(self):
+        self.draw.rectangle([(0,0),self.dim],fill=255) # clear image
+
+    def text(self,loc,text,font=None):
+        if not font:
+            font = self.font_txt
+        self.draw.text(loc,text,font=font,fill=0)
+    
+    def show_ready(self,path = None):
+        self.clear()
+        self.text((0,0),'gronk is ready.', font=self.font_logo)
+        if path:
+            self.text((0,25),f'SET prints {path}')
+        else:
+            self.text((0,25),'Arrow keys to jog')
+        self.text((0,45),'TEST toggles pen up/down')
+        self.text((0,65),'Upload files at http://gronk.lan/')
+        self.text((15,85),'Hold "ONLINE" button')
+        self.text((15,105),'for 5 seconds to shut down.')
+        self.update()
+    
+
+
+    def show_goodbye(self):
+        self.clear()
+        self.text((10,5),'goodbye, gronk.', font=font_logo)
+        self.text((0,40),'Please wait thirty seconds before')
+        self.text((0,60),'switching off.')
+        self.text((0,80),'Expect to wait about 120 seconds for')
+        self.text((0,100),'gronk to boot after turning back on.')
+        self.update()
+
+
